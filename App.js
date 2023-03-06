3dbfccc0a40d08880f6c9b992a223ad3255c3a09
anhhtu <vuanhtu0711@gmail.com>
2023-03-06 16:10:48 +0700

    first push

---
+++ App.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+export default function App() {
+  return (
+    <HomeScreen/>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native'
+import React from 'react'
+import Header from '../components/Header'
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Header/>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+
+  }
+})
